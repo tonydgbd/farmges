6baf439f60812a9de34a97cee147f653eee3bff1
Ouedraogo John <johnodg369@gmail.com>
2023-09-02 17:24:28 +0000

    updated

--- lib/views/widgets/date_field.dart
+++ lib/views/widgets/date_field.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 
-Widget DateInputField(Function getValue) {
+Widget DateInputField(TextEditingController controller, BuildContext context) {
+  DateTime dateSelected = DateTime.now();
+ 
+  openCallendar() async {
+    var initialDate = DateTime.now();
+    var firstDate = DateTime.now();
+    var lastDate = DateTime(2024, 12, 12, 12, 12);
+    var date = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: firstDate,
+        lastDate: lastDate);
+    controller.text = date!.toString();
+    print(date);
+    dateSelected = date;
+  }
+
   return Padding(
-    padding: const EdgeInsets.all(20.0),
-    child: TextField(
-      keyboardType: TextInputType.datetime,
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        hintText: 'Choisir une date',
-        labelText: 'Date',
-      ),
-      onChanged: (value) => getValue(value),
-    ),
-  );
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          IconButton(
+              onPressed: openCallendar, icon: const Icon(Icons.calendar_today)),
+          Text("choisir une date $dateSelected"),
+          
+        ],
+      )
+      //
+      );
+}
+
+mixin class DateInputMixin {
+  TextEditingController dateController = TextEditingController();
+
+  getDate() {
+    // return dateController.text;
+    return DateTime.now();
+  }
+
+  clearDate() {
+    dateController.clear();
+  }
+
+  Widget DateInput(BuildContext context) {
+    return DateInputField(dateController, context);
+  }
 }
